6dc6becca1074758d85dd4dc48b89f5998cd4341
Francis Nuako <francisnuako@gmail.com>
2020-07-12 14:32:11 +0000

    registration form done

--- resources/views/auth/new_signup.blade.php
+++ resources/views/auth/new_signup.blade.php
@@ -2,19 +2,31 @@
 
 @section('contents')
 <div class="header">
-	<h1>user profile form</h1>
+	<h1>Get Hooked</h1>
 </div>
 <div class="w3-main">
 		<div class="form-w3l">
-			<div class="img">
-				<img src="images/profile.jpg" alt="image">
-				<h2>signup here</h2>
-			</div>	
+
 			<form action="#" method="post">
 				<div class="w3l-user">
 					<span><i class="fa fa-user-circle-o w3l-1" aria-hidden="true"></i></span>
-					<input type="text" name="username" placeholder="username" required=""/>
+					<input type="text" name="name" placeholder="name" required=""/>
 					<div class="clear"></div>
+				</div>
+				<div class="w3l-user">
+					<span><i class="fa fa-child w3l-1" aria-hidden="true"></i></span>
+					<input type="text" name="age" placeholder="age" required=""/>
+					<div class="clear"></div>
+				</div>
+				<div class="w3l-user">
+					<span><i class="fa fa-industry w3l-2" aria-hidden="true"></i></span>
+
+					<select class="form-control"> 
+							<option>Occupation</option>
+							<option>Banker</option>
+
+					</select>
+
 				</div>
 				
 				<div class="w3l-password">
@@ -22,18 +34,43 @@
 					<input type="password" name="password" placeholder="password" required=""/>
 					<div class="clear"></div>
 				</div>
+				<div class="w3l-password">
+						<span><i class="fa fa-lock w3l-2" aria-hidden="true"></i></span>
+						<input type="password" name="confirm_password" placeholder="Confirm password" required=""/>
+						<div class="clear"></div>
+					</div>
+	
+
+				<div class="w3l-password">
+					<span><i class="fa fa-female" aria-hidden="true">
+					</i></span> 
+					<label for="male">Male</label>
+					<input class="form-check-input w3l-2" type="radio" name="Male" value="Male" required=""/>
+					<label for="female">Female</label>
+					<input class="form-check-input w3l-2" type="radio" name="Male" value="Female" required=""/>
+				</div>
 				
 				<div class="w3l-email">
 					<span><i class="fa fa-envelope-o w3l-3" aria-hidden="true"></i></span>
-					<input type="email" name="e-mail" placeholder="info@example.com" required=""/>
+					<input type="email" name="e-mail" placeholder="yourname@gmail.com" required=""/>
 					<div class="clear"></div>
 				</div>
 				
 				<div class="w3l-phone">	
 					<span><i class="fa fa-mobile w3l-4" aria-hidden="true"></i></span>
-					<input type="text" name="phone" placeholder="+001" required=""/>
+					<input type="text" name="phone" placeholder="0200 00 00 00" required=""/>
 					<div class="clear"></div>
 				</div>
+
+				<div class="w3l-user">	
+						<select class="form-control"> 
+								<option>Interested In?</option>
+								<option></option>
+	
+						</select>
+	
+					</div>
+
 				
 				<div class="w3l-btn">
 					<input type="submit" name="button" value="sign up"/>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,12 +16,20 @@
 function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Meta tag Keywords -->
 <!-- css files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
-<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link href="{{asset('auth/css/style.css')}}" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="{{asset('auth/css/font-awesome.css')}}"> <!-- Font-Awesome-Icons-CSS -->
 <!-- //css files -->
 <!-- online-fonts -->
 <link href="//fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!--//online-fonts -->
 </head>
 <body>
