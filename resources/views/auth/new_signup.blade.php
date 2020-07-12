6c2186d03fc8e672de3986db15636588589c8763
Francis Nuako <francisnuako@gmail.com>
2020-07-12 15:29:51 +0000

    added profile pic field to form

--- resources/views/auth/new_signup.blade.php
+++ resources/views/auth/new_signup.blade.php
@@ -1,82 +1,94 @@
 @extends('layouts.auth')
 
 @section('contents')
-<div class="header">
-	<h1>Get Hooked</h1>
-</div>
-<div class="w3-main">
-		<div class="form-w3l">
-
-			<form action="#" method="post">
-				<div class="w3l-user">
-					<span><i class="fa fa-user-circle-o w3l-1" aria-hidden="true"></i></span>
-					<input type="text" name="name" placeholder="name" required=""/>
-					<div class="clear"></div>
-				</div>
-				<div class="w3l-user">
-					<span><i class="fa fa-child w3l-1" aria-hidden="true"></i></span>
-					<input type="text" name="age" placeholder="age" required=""/>
-					<div class="clear"></div>
-				</div>
-				<div class="w3l-user">
-					<span><i class="fa fa-industry w3l-2" aria-hidden="true"></i></span>
-
-					<select class="form-control"> 
-							<option>Occupation</option>
-							<option>Banker</option>
-
-					</select>
-
-				</div>
-				
-				<div class="w3l-password">
-					<span><i class="fa fa-lock w3l-2" aria-hidden="true"></i></span>
-					<input type="password" name="password" placeholder="password" required=""/>
-					<div class="clear"></div>
-				</div>
-				<div class="w3l-password">
-						<span><i class="fa fa-lock w3l-2" aria-hidden="true"></i></span>
-						<input type="password" name="confirm_password" placeholder="Confirm password" required=""/>
-						<div class="clear"></div>
+<body>
+		<h1 class="header-w3ls">
+		   Sign Up and Get Hooked
+		</h1>
+		<div class="mid-cls">
+		   <form action="#" method="post">
+			  <div class="main">
+				 <div class="icon-head-wthree">
+					<div class="images-headder">
+					   <div class="wrap-custom-file">
+						  <input type="file" name="image1" id="image1" accept=".gif, .jpg, .png" />
+						  <label  for="image1">
+						  <span><img src="images/t1.jpg" alt="" class="image-fluid"></span>
+						  <span class="fas fa-camera-retro"></span>
+						  </label>
+					   </div>
 					</div>
-	
-
-				<div class="w3l-password">
-					<span><i class="fa fa-female" aria-hidden="true">
-					</i></span> 
-					<label for="male">Male</label>
-					<input class="form-check-input w3l-2" type="radio" name="Male" value="Male" required=""/>
-					<label for="female">Female</label>
-					<input class="form-check-input w3l-2" type="radio" name="Male" value="Female" required=""/>
-				</div>
-				
-				<div class="w3l-email">
-					<span><i class="fa fa-envelope-o w3l-3" aria-hidden="true"></i></span>
-					<input type="email" name="e-mail" placeholder="yourname@gmail.com" required=""/>
-					<div class="clear"></div>
-				</div>
-				
-				<div class="w3l-phone">	
-					<span><i class="fa fa-mobile w3l-4" aria-hidden="true"></i></span>
-					<input type="text" name="phone" placeholder="0200 00 00 00" required=""/>
-					<div class="clear"></div>
-				</div>
-
-				<div class="w3l-user">	
-						<select class="form-control"> 
-								<option>Interested In?</option>
-								<option></option>
-	
-						</select>
-	
+					<div class="personal-info">
+					   <p>Gender</p>
+					   <div class="form-check">
+						  <input class="form-check-input" type="radio" name="select4" value="option1" checked="">
+						  <label class="form-check-label">
+						  Male
+						  </label>
+					   </div>
+					   <div class="form-check">
+						  <input class="form-check-input" type="radio" name="select4" value="option2" checked="">
+						  <label class="form-check-label">
+						  Female
+						  </label>
+					   </div>
 					</div>
-
-				
-				<div class="w3l-btn">
-					<input type="submit" name="button" value="sign up"/>
-				</div>
-			</form>
+				 </div>
+				 <div class="form-left-to-w3l">
+					<input type="text" name="name" placeholder="Name" required="">
+					<div class="clear"></div>
+				 </div>
+				 <div class="form-left-to-w3l">
+					<input type="text" name="name" placeholder="Last Name" required="">
+					<div class="clear"></div>
+				 </div>
+				 <div class="form-left-w3l">
+					<input type="email" name="email" placeholder="Email" required="">
+				 </div>
+				 <div class="form-right-w3ls ">
+					<input type="password" name="password" placeholder="Password" required="">
+					<div class="clear"></div>
+				 </div>
+				 <div class="btnn">
+					<button type="submit">Sign Up</button><br>
+					<div class="clear"></div>
+				 </div>
+			  </div>
+		   </form>
 		</div>
-	</div>
+		<div class="copy">
+		   <p>&copy;2018 Solo profile Form. All Rights Reserved | Design by <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a></p>
+		</div>
+		<!--js working-->
+		<script src='js/jquery-2.2.3.min.js'></script>
+		<!--//js working-->
+		<script>
+		   $('input[type="file"]').each(function(){
+			 // Refs
+			 var $file = $(this),
+				 $label = $file.next('label'),
+				 $labelText = $label.find('span'),
+				 labelDefault = $labelText.text();
+		   
+			 // When a new file is selected
+			 $file.on('change', function(event){
+			   var fileName = $file.val().split( '\\' ).pop(),
+				   tmppath = URL.createObjectURL(event.target.files[0]);
+			   //Check successfully selection
+			   if( fileName ){
+				 $label
+				   .addClass('file-ok')
+				   .css('background-image', 'url(' + tmppath + ')');
+				 $labelText.text(fileName);
+			   }else{
+				 $label.removeClass('file-ok');
+				 $labelText.text(labelDefault);
+			   }
+			 });
+		   
+		   // End loop of file input elements
+		   });
+		</script>
+	 </body>
 	
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,10 +18,15 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- css files -->
 <link href="{{asset('auth/css/style.css')}}" rel="stylesheet" type="text/css" media="all">
 <link rel="stylesheet" href="{{asset('auth/css/font-awesome.css')}}"> <!-- Font-Awesome-Icons-CSS -->
+<link href="{{asset('css/fontawesome-all.min.css')}}" rel="stylesheet" type="text/css" media="all">
+<link href="{{asset('auth/css/style1.css')}}" rel='stylesheet' type='text/css' media="all">
+
 <!-- //css files -->
 <!-- online-fonts -->
 <link href="//fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -31,6 +36,8 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!--//online-fonts -->
+
+
 </head>
 <body>
 @yield('contents')
