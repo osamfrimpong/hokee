cc64844f81393508da7956211c7f8855e256ffda
Francis Nuako <francisnuako@gmail.com>
2020-09-12 23:42:45 +0000

    additional pages

---
+++ resources/views/contactUs.blade.php
@@ -0,0 +1,355 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>{{config('app.name')}} - Contact Us</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
+<link href="{{asset('css/font-awesome.css')}}" rel="stylesheet"> 
+<link rel="stylesheet" href="{{asset('css/chocolat.css')}}" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="{{asset('css/flexslider.css')}}" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<!-- header -->
+<div class="banner-top">
+			<div class="social-bnr-agileits">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+			</div>
+			<div class="contact-bnr-w3-agile">
+				<ul>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:hookeegh@gmail.com">hookeegh@gmail.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>+233 200 000 000</li>	
+					<li class="s-bar">
+						<div class="search">
+							<input class="search_box" type="checkbox" id="search_box">
+							<label class="icon-search" for="search_box"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></label>
+							<div class="search_form">
+								<form action="#" method="post">
+									<input type="search" name="Search" placeholder=" " required=" " />
+									<input type="submit" value="Search">
+								</form>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="#"><p class="logo_w3l_agile_caption">Feel Some Intimacy</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class=""><a href="{{route('home')}}" class="">Home</a></li>
+							<li class=""><a href="{{route('about')}}" class="">About</a></li>
+							<li class=""><a href="{{route('hotels')}}" class="">Partner Hotels</a></li>
+							<li class=""><a href="{{route('enhancers')}}" class="">Sexual Enhancers</a></li>
+							<li class=""><a href="{{route('contactUs')}}" class="">Contact Us</a></li>
+
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+<!-- //header -->
+		<!-- banner -->
+	
+	<!-- //banner --> 
+<!--//Header-->
+
+<!-- banner-bottom -->
+	<div class="banner-bottom">
+		<div class="container">	
+			<div class="agileits_banner_bottom">
+				<h3><span>Get Hooked Up to a male or female , enjoy fantastic services</span> Connecting people who want to feel some intimacy.
+				<br>Connect and advertise hotel systems <br>
+				Sell sex enhancers and toys  </h3>
+			</div>
+			<div class="w3ls_banner_bottom_grids">
+				<ul class="cbp-ig-grid">
+					<li>
+						<div class="w3_grid_effect">
+							<span class="cbp-ig-icon w3_road"></span>
+							<h4 class="cbp-ig-title">MASSAGE</h4>
+							<span class="cbp-ig-category">Hookee GH</span>
+						</div>
+					</li>
+					<li>
+						<div class="w3_grid_effect">
+							<span class="cbp-ig-icon w3_cube"></span>
+							<h4 class="cbp-ig-title">ONE NIGHT STAND</h4>
+							<span class="cbp-ig-category">Hookee GH</span>
+						</div>
+					</li>
+					<li>
+						<div class="w3_grid_effect">
+							<span class="fa fa-female fa-4x"></span>
+							<h4 class="cbp-ig-title">RENT A BOYFRIEND</h4>
+							<span class="cbp-ig-category">Hookee GH</span>
+						</div>
+					</li>
+					<li>
+						<div class="w3_grid_effect">
+							<span class="fa fa-male fa-4x"></span>
+							<h4 class="cbp-ig-title">RENT A GIRLFRIEND</h4>
+							<span class="cbp-ig-category">Hookee GH</span>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!-- //banner-bottom -->
+<!-- /about -->
+ 	<div class="about-wthree" id="about">
+		  <div class="container">
+				   <div class="ab-w3l-spa">
+                            <h3 class="title-w3-agileits title-black-wthree">About Hookee GH</h3> 
+						   <p style="font-size: 25px; text-align: left;" class="about-para-w3ls">HooKee is a Ghanaian based, internationally operating an online hook-up, sexual content display, sexual health education and social networking website. <br>
+							HooKee features filling a statement or requesting form called "Brief" to match members who want some intimacy.
+						<br>
+						At HooKee, requesting member's data is advertised, concealing the name, address and phone details and only given to the interested HooKee, who has paid for such information.<br>
+					HooKee operates two types of accounts; Regular and VIP. Regular HooKee is not charged for account creation, but VIP HooKee is charged $20 for account creation (to be renewed quarterly). The  VIP account holder enjoys the privilege of getting to see "requesting HooKee's" info one hour before a Regular HooKee gets to see it on the website. </p>
+						   <p style="font-size: 25px; text-align: left;" class="about-para-w3ls"> HooKee operates two types of accounts; Regular and VIP. Regular HooKee is not charged for account creation, but VIP HooKee is charged $20 for account creation (to be renewed quarterly). The  VIP account holder enjoys the privilege of getting to see "requesting HooKee's" info one hour before a Regular HooKee gets to see it on the website. 
+						<br> HooKee also advertises hotel systems and sex enhancement devices like dildos, vibrators, condoms, penis enlargement devices among others, which are sold at a fee. 
+					<br> HooKee operates an embedded blog that holds sexual contents and write-ups that educate on sexual lifestyle.</p>
+
+		          </div>
+		   	<div class="clearfix"> </div>
+    </div>
+</div>
+ 	<!-- //about -->
+<!--sevices-->
+
+<!--//sevices-->
+<!-- team -->
+
+<!-- //team -->
+<!-- Gallery -->
+
+<!-- //gallery -->
+
+
+
+	 <!-- services & rates -->
+     
+	 <!--// services & rates -->
+  <!-- visitors -->
+	
+  <!-- visitors -->
+<!-- contact -->
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contact Us</h4>
+				<p class="contact-agile2">Sign Up For Our News Letters</p>
+				<form action="#" method="post" name="sentMessage" id="contactForm" novalidate>
+					<div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Full Name:</label>
+                            <input type="text" class="form-control" name="name" id="name" required data-validation-required-message="Please enter your name.">
+                            <p class="help-block"></p>
+                        </div>
+                    </div>	
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required data-validation-required-message="Please enter your phone number.">
+							<p class="help-block"></p>
+						</div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Email Address:</label>
+                            <input type="email" class="form-control" name="email" id="email" required data-validation-required-message="Please enter your email address.">
+							<p class="help-block"></p>
+						</div>
+                    </div>
+                    <div id="success"></div>
+                    <!-- For success/fail messages -->
+                    <button type="submit" class="btn btn-primary">Send</button>	
+				</form>            
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong> +1 (100)222-23-33</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">hookeegh@gmail.com</a></p>
+																
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+<!-- /contact -->
+			<div class="copy">
+		        <p>© 2020 Hookee GH . All Rights Reserved | Design by <a href="https://pcbossonline.com/">PCB Technologies</a> </p>
+		    </div>
+<!--/footer -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- contact form -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>	
+<!-- /contact form -->	
+<!-- Calendar -->
+		<script src="js/jquery-ui.js"></script>
+		<script>
+				$(function() {
+				$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+				});
+		</script>
+<!-- //Calendar -->
+<!-- gallery popup -->
+<link rel="stylesheet" href="css/swipebox.css">
+				<script src="js/jquery.swipebox.min.js"></script> 
+					<script type="text/javascript">
+						jQuery(function($) {
+							$(".swipebox").swipebox();
+						});
+					</script>
+<!-- //gallery popup -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- flexSlider -->
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			<!-- //flexSlider -->
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						// You can also use "$(window).load(function() {"
+						$(function () {
+						  // Slideshow 4
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							timeout: 11000,
+							pager:false,
+							nav:true,
+							speed: 1500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+					
+						});
+			</script>
+		<!--search-bar-->
+		<script src="js/main.js"></script>	
+<!--//search-bar-->
+<!--tabs-->
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+$(document).ready(function () {
+$('#horizontalTab').easyResponsiveTabs({
+type: 'default', //Types: default, vertical, accordion           
+width: 'auto', //auto or any width like 600px
+fit: true,   // 100% fit in a container
+closed: 'accordion', // Start closed if in accordion view
+activate: function(event) { // Callback function if tab is switched
+var $tab = $(this);
+var $info = $('#tabInfo');
+var $name = $('span', $info);
+$name.text($tab.text());
+$info.show();
+}
+});
+$('#verticalTab').easyResponsiveTabs({
+type: 'vertical',
+width: 'auto',
+fit: true
+});
+});
+</script>
+<!--//tabs-->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	
+	<div class="arr-w3ls">
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	</div>
+<!-- //smooth scrolling -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
